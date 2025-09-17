f4ad93868a8ad00982644ec3aaad75df9f3bcfb0
Dexie's babies <gorvengarciasalaveria@gmail.com>
2025-09-18 00:06:22 +0800

    updated

--- routes/employees.js
+++ routes/employees.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../config/db");
+const db = require("../dbconfig.js");
 
 // 📌 Get all employees with department info
 router.get("/", async (req, res) => {
